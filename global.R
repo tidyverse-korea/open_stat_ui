4217891363a372a857f9182c5177efa39701f3b7
Kwnaghcun Lee <kwangchun.lee.7@gmail.com>
2021-09-02 17:40:19 +0900

    fix ui

--- global.R
+++ global.R
@@ -13,3 +13,30 @@ details_list_columns <- tibble(
     key = fieldName)
 
 
+
+# 카드 제작 함수 ----------------------------------
+
+makeCard <- function(title, content, size = 12, style = "") {
+    div(
+        class = glue::glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
+        style = style,
+        Stack(
+            tokens = list(childrenGap = 5),
+            Text(variant = "large", title, block = TRUE),
+            content
+        )
+    )
+}
+
+# 페이지 제작 함수 ----------------------------------
+
+makePage <- function (title, subtitle, contents) {
+    tagList(div(
+        class = "page-title",
+        span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style =
+                 "color: #323130"),
+        span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style =
+                 "color: #605E5C; margin: 14px;")
+    ),
+    contents)
+}
--- ui.R
+++ ui.R
@@ -1,31 +1,4 @@
 
-# 카드 제작 함수 ----------------------------------
-
-makeCard <- function(title, content, size = 12, style = "") {
-    div(
-        class = glue::glue("card ms-depth-8 ms-sm{size} ms-xl{size}"),
-        style = style,
-        Stack(
-            tokens = list(childrenGap = 5),
-            Text(variant = "large", title, block = TRUE),
-            content
-        )
-    )
-}
-
-# 페이지 제작 함수 ----------------------------------
-
-makePage <- function (title, subtitle, contents) {
-    tagList(div(
-        class = "page-title",
-        span(title, class = "ms-fontSize-32 ms-fontWeight-semibold", style =
-                 "color: #323130"),
-        span(subtitle, class = "ms-fontSize-14 ms-fontWeight-regular", style =
-                 "color: #605E5C; margin: 14px;")
-    ),
-    contents)
-}
-
 # 제어를 위한 필터 ---------------------------------
 
 filters <- Stack(
